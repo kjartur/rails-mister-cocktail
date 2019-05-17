014303ae8a338bc9780e3eeac844b7589f462e55
Kjartan Manvelyan <kjartan.manvelyan@gmail.com>
2019-05-17 11:30:04 +0200

    Configured routes and generated Cocktail controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  root to: 'cocktails#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #get '/cocktails', to: 'cocktails#index'"
+  # delete '/restaurants/:id', to: 'restaurants#destroy'
+  # post '/restaurants', to: 'restaurants#create'"
+  # get '/restaurants/new', to: 'restaurants#new'"
+  # get '/restaurants/:id/edit', to: 'restaurants#edit'"
+  # get '/restaurants/:id', to: 'restaurants#show'"
+  # patch '/restaurants/:id', to: 'restaurants#update'"
+  resources :cocktails, only: [:index, :show, :new, :create]
+
 end
