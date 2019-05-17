1c231d3e6e52d62458ef17acff40241930c45ffc
Kjartan Manvelyan <kjartan.manvelyan@gmail.com>
2019-05-18 01:03:12 +0200

    Changed routes, corrected links in navbar, changed show and new.html.erb

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,22 +1,35 @@
 class DosesController < ApplicationController
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+    @ingredients = Ingredient.all
+  end
+
   def create
-    @dose = Dose.new(dose_params)
-    @cocktail = Cocktail.find(params[:id])
+    # create cocktail instance
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    # create doses instance with ingredient instance???
+    @dose = Dose.new(doses_params)
+    # tell doses instance that it belongs to cocktail instance
     @dose.cocktail = @cocktail
+    # check if doses instance was saved
     if @dose.save
       redirect_to cocktail_path(@cocktail)
     else
-      render "cocktails/show"
+      render :new
     end
   end
 
   def destroy
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.find(params[:id])
+    @dose.delete
+    redirect_to cocktail_path(@cocktail)
   end
 
   private
 
-  def cocktail_params
-    params.require(:dose).permit(:description, :indgredient_id)
+  def doses_params
+    params.require(:dose).permit(:description, :ingredient_id)
   end
-
 end
